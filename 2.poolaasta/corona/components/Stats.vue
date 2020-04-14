068659ca208308f6eda044b27ec7897c54d1ff5e
Georg Daniel Vahtramäe <48998862+GeorgDV@users.noreply.github.com>
2020-04-14 13:23:31 +0300

    Add corona

---
+++ 2.poolaasta/corona/components/Stats.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="card">
+    <header class="card-header has-background-grey-lighter">
+      <p class="card-header-title">
+        {{data.Country}}
+      </p>
+      <img v-if="data.CountryCode" :src="flagUrl">
+    </header>
+    <div class="card-content has-background-light">
+      <table>
+        <tbody>
+          <tr v-for="stat in allStats">
+            <td >{{stat}}</td>
+            <td class="has-text-right">{{data[stat]}}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "Stats",
+        props: ['data'],
+        data() {
+          return {
+            stats: ['Confirmed', 'Deaths', 'Recovered']
+          }
+        },
+        computed: {
+          flagUrl() {
+            return `https://www.countryflags.io/${this.data.CountryCode.toLowerCase()}/flat/64.png`;
+          },
+          totalStats(){
+            return this.stats.map(stat => 'Total' + stat);
+          },
+          newStats(){
+            return this.stats.map(stat => 'New' + stat);
+          },
+          allStats(){
+            let all = [];
+            all.push(...this.totalStats);
+            all.push(...this.newStats);
+            return all;
+          }
+        }
+    }
+</script>
+
+<style scoped>
+  table {
+    width: 100%;
+  }
+
+  .card-header {
+    padding: 0.4em;
+  }
+</style>
